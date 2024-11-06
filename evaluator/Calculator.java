58dfbbd4ee8a6af3f6c2d6bce1bcffd59345ffa6
madaramebak <zakariahas07@gmail.com>
2024-11-05 21:08:44 -0500

    Adding Paren/Exp evaluation tmrw

--- evaluator/Calculator.java
+++ evaluator/Calculator.java
@@ -1,13 +1,6 @@
 package grind.evaluator;
 
 public class Calculator {
-    public void canHandle(TokenType type){
-        if(type == TokenType.NUMBER){
-            System.out.println("Can handle Numbers");
-        }else if(type == TokenType.MINUS || type == TokenType.PLUS) {
-            System.out.println("I need Numbers not just operators");
-        }
-    }
 
     public static double addition(double num1, double num2){
         return num1 + num2;
